cda175453d8a54d362997442ed29c950e4ca8f59
Fèlix Arribas Pardo <felixarpa@gmail.com>
2017-06-19 18:32:13 +0100

    tasca #5 finalitzada

--- waslab05/WASLab05_SimpleTwitterClient/src/twitter/SimpleClient.java
+++ waslab05/WASLab05_SimpleTwitterClient/src/twitter/SimpleClient.java
@@ -2,6 +2,8 @@ package twitter;
 
 import java.util.Date;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
@@ -12,9 +14,26 @@ public class SimpleClient {
 		
 		final Twitter twitter = new TwitterFactory().getInstance();
 		
+		/*
 		Date now = new Date();
 		String latestStatus = "I want to increase the Klout score of @cfarre [task #4 completed "+now+"]";
 		Status status = twitter.updateStatus(latestStatus);
-		System.out.println("Successfully updated the status to: " + status.getText());       
+		System.out.println("Successfully updated the status to: " + status.getText());
+		*/
+		
+		Query query1 = new Query();
+		query1.setQuery("@fib_was");
+		query1.setResultType(Query.RECENT);
+		query1.setCount(1);
+		QueryResult result1 = twitter.search(query1);
+		
+		Status aswTweet = result1.getTweets().get(0);
+		
+		System.out.println(aswTweet.getText());
+		
+		twitter.retweetStatus(aswTweet.getId());
+		
+		
+		
 	}
 }
