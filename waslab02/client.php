ceee2e01c4368e213141a08db895cbcf2b5ed6fa
Fèlix Arribas Pardo <felixarpa@gmail.com>
2017-06-19 18:05:56 +0100

    tasca #3 finalitzada

--- waslab02/client.php
+++ waslab02/client.php
@@ -3,6 +3,16 @@
 $URI = 'http://localhost:8080/waslab02/wall.php';
 $resp = file_get_contents($URI);
 echo $http_response_header[0], "\n"; // Print the first HTTP response header
-echo $resp;  // Print HTTP response body
+//echo $resp;  // Print HTTP response body
+$tweets = new SimpleXMLElement($resp);
+foreach ($tweets->tweet as $tweet) {
+    $id = (string) $tweet["id"];
+    $author = $tweet->author;
+    $text = $tweet->text;
+    $time = $tweet->time;
+    echo "[tweet #" . $id . "] " . $author . ": " . $text . ". [" . $time . "]\n";
+    // [tweet #7] Donald: When somebody challenges you, fight back. Be brutal, be tough. [2017-02-22T16:55:58+00:00]
+    
+}
 
 ?>
